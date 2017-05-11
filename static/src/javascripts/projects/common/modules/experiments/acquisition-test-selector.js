1026e92b74463349399ac92bd1c6c03380e23291
Sam Desborough <sam.desborough@gmail.com>
2017-05-11 16:25:26 +0100

    transform projects/common/modules/experiments/acquisition-test-selector.js to ES6 module

--- static/src/javascripts/projects/common/modules/experiments/acquisition-test-selector.js
+++ static/src/javascripts/projects/common/modules/experiments/acquisition-test-selector.js
@@ -1,69 +1,54 @@
-define([
-    'lodash/collections/reduce',
-    'common/modules/experiments/segment-util',
-    'common/modules/experiments/test-can-run-checks',
-    'common/modules/commercial/acquisitions-view-log',
-    'common/modules/experiments/tests/contributions-epic-always-ask-strategy',
-    'common/modules/experiments/tests/contributions-epic-ask-four-earning',
-    'common/modules/experiments/tests/acquisitions-epic-liveblog',
-    'common/modules/experiments/tests/acquisitions-epic-testimonials-round-two',
-    'common/modules/experiments/tests/acquisitions-epic-single-cta'
+import reduce from 'lodash/collections/reduce';
+import segmentUtil from 'common/modules/experiments/segment-util';
+import testCanRunChecks from 'common/modules/experiments/test-can-run-checks';
+import viewLog from 'common/modules/commercial/acquisitions-view-log';
+import alwaysAsk from 'common/modules/experiments/tests/contributions-epic-always-ask-strategy';
+import askFourEarning from 'common/modules/experiments/tests/contributions-epic-ask-four-earning';
+import acquisitionsEpicLiveBlog from 'common/modules/experiments/tests/acquisitions-epic-liveblog';
+import acquisitionsEpicTestimonials from 'common/modules/experiments/tests/acquisitions-epic-testimonials';
 
-], function (
-    reduce,
-    segmentUtil,
-    testCanRunChecks,
-    viewLog,
+/**
+ * acquisition tests in priority order (highest to lowest)
+ */
+var tests = [
+    acquisitionsEpicTestimonials,
     alwaysAsk,
     askFourEarning,
-    acquisitionsEpicLiveBlog,
-    acquisitionsEpicTestimonialsRoundTwo,
-    acquisitionsEpicSingleCta
-) {
-    /**
-     * acquisition tests in priority order (highest to lowest)
-     */
-    var tests = [
-        alwaysAsk,
-        acquisitionsEpicSingleCta,
-        acquisitionsEpicTestimonialsRoundTwo,
-        askFourEarning,
-        acquisitionsEpicLiveBlog
-    ];
+    acquisitionsEpicLiveBlog
+];
 
-    var epicEngagementBannerTests = reduce(tests, function(out, test) {
-        var testInstance = new test();
-        if (testInstance.isEngagementBannerTest) {
-            out.push(testInstance)
-        }
-        return out;
-    }, []);
+var epicEngagementBannerTests = reduce(tests, function(out, test) {
+    var testInstance = new test();
+    if (testInstance.isEngagementBannerTest) {
+        out.push(testInstance)
+    }
+    return out;
+}, []);
 
-    var abTestClashData = tests.map(function(test) {
-        return new test();
-    });
+var abTestClashData = tests.map(function(test) {
+    return new test();
+});
 
-    return {
+export default {
 
-        epicEngagementBannerTests: epicEngagementBannerTests,
+    epicEngagementBannerTests: epicEngagementBannerTests,
 
-        abTestClashData: abTestClashData,
+    abTestClashData: abTestClashData,
 
-        getTest: function() {
-            var eligibleTests = tests.filter(function (test) {
-                var t = new test();
-                var forced = window.location.hash.indexOf('ab-' + t.id) > -1;
-                var variant = segmentUtil.variantFor(t);
+    getTest: function() {
+        var eligibleTests = tests.filter(function(test) {
+            var t = new test();
+            var forced = window.location.hash.indexOf('ab-' + t.id) > -1;
+            var variant = segmentUtil.variantFor(t);
 
-                var hasNotReachedRateLimit = variant &&
-                    ((viewLog.viewsInPreviousDays(variant.maxViews.days) < variant.maxViews.count &&
-                    viewLog.viewsInPreviousDays(variant.maxViews.minDaysBetweenViews) === 0) ||
+            var hasNotReachedRateLimit = variant &&
+                ((viewLog.viewsInPreviousDays(variant.maxViews.days) < variant.maxViews.count &&
+                        viewLog.viewsInPreviousDays(variant.maxViews.minDaysBetweenViews) === 0) ||
                     variant.isUnlimited);
 
-                return forced || (testCanRunChecks.testCanBeRun(t) && segmentUtil.isInTest(t) && hasNotReachedRateLimit);
-            });
+            return forced || (testCanRunChecks.testCanBeRun(t) && segmentUtil.isInTest(t) && hasNotReachedRateLimit);
+        });
 
-            return eligibleTests[0] && new eligibleTests[0]();
-        }
+        return eligibleTests[0] && new eligibleTests[0]();
     }
-});
+}
